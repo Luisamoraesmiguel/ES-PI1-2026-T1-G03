26225aa8bdc1e1e6169201d69a37bbf94c5b0b37
Sabrina Prates <sabrina.ps@puccampinas.edu.br>
2026-04-07 15:26:11 -0300

    Alteração no banco de dados e na conexão py-sql.

--- teste_conexao.py
+++ teste_conexao.py
@@ -4,18 +4,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-mydb = mysql.connector.connect(
+conexao = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_DATABASE")
 )
-cursor = mydb.cursor()
+cursor = conexao.cursor()
 
-cpf = str(input("Digite o CPF: "))
-nome = str(input("Digite o nome: "))
-cursor.execute(f"INSERT into teste (cpf, nome) VALUES ('{cpf}', '{nome}')")
-mydb.commit()
-cursor.execute("select * from teste")
-table = cursor.fetchall()
-print(table)
+sql = "INSERT INTO eleitores (nome, cpf, titulo, mesario, votou, chave_de_acesso) VALUES (%s, %s, %s, %s, %s, %s)"
+valores = ("João Silva", "12345678900", "123456789012", 'N', 'N', 'chave123')
+cursor.execute(sql, valores)
+conexao.commit()
+
+print("Cadastrado com sucesso!")
+
+    
+    
+
+
+
+
+
+
+
