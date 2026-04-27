7bff05366cdd0d495df1fac26744b47b951349df
Sabrina Prates <sabrina.ps@puccampinas.edu.br>
2026-04-27 10:27:32 -0300

    tentativa de solucao para validacao do cpf do mesario

--- Códigos_fonte/validacoes/mesario.py
+++ Códigos_fonte/validacoes/mesario.py
@@ -12,10 +12,28 @@ def verificar_mesario(titulo, cpf_4digitos, chave):
     cursor = conexao.cursor()
     
     # Comando SQL para verificar se o mesário existe no banco de dados
-    sql = f"SELECT cpf, mesario FROM eleitores WHERE titulo = %s AND chave_de_acesso = %s AND mesario = 'S'"
+    sql = f"SELECT cpf, mesario FROM eleitores WHERE titulo = %s AND chave_de_acesso = %s "
     
     cursor.execute(sql, (titulo, chave)) # Executa o comando SQL
     resultado = cursor.fetchone() # Pega o resultado da consulta
+
+    if resultado:
+        cpf_banco, is_mesario = resultado # Pega o CPF e se é mesário do resultado
+        if is_mesario != 'S': # Verifica se é mesário
+            print('Acesso negado: O título fornecido não pertence a um mesário.')
+            return False
+        cpf_banco_str = str(cpf_banco) # Converte o CPF do banco de dados para string
+        if cpf_banco_str.endwith(str(cpf_4digitos)): # Verifica se os 4 últimos dígitos do CPF no banco de dados correspondem aos 4 dígitos fornecidos pelo usuário
+            print('Acesso concedido: Mesário verificado com sucesso.')
+            return True
+        else:
+            print('Cpf invalido.')
+            return False
+    else:
+        print('Título ou chave de acesso inválidos.')
+        return False
+    
+ 
 
     cursor.close() # Fecha o cursor
     conexao.close() # Fecha a conexão com o banco de dados
