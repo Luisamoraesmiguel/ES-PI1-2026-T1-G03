b14821f3a05748524ebc5ee22539c6d1afe0696c
Valadares19 <beatriz.bv2@puccampinas.edu.br>
2026-04-27 18:38:48 -0300

    Finalizando submodulo de voto com conexao MySQL

---
+++ ES-PI1-2026-T1-G03-main/Códigos_fonte/submodulo_voto.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+def realizar_votacao():
+    conexao= mysql.connector.connect(
+        host="localhost", 
+        user="root", 
+        password="Beatriz190524", 
+        database="tabela_bd")
+    
+    cursor = conexao.cursor()
+
+    titulo=input("Digite o seu Título de Eleitor: ")
+    voto=input("Digite o número do seu candidato: ")
+
+    #Comando pro Banco de dados
+    comando_sql = "INSERT INTO votos, titulo, numero, data_voto) VALUES (%s, %s, NOW())"
+    dados_voto = (titulo, voto)
+    cursor.execute(comando_sql, dados_voto)
+    conexao.commit()
+    print(f"Voto confirmado para o candidato {voto}!")
+    cursor.close()
+    conexao.close()
+realizar_votacao()
