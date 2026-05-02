5320102d9d8a2e83ae90fc567f661e71ab45182d
Luisamoraesmiguel <luisa.mm1@puccampinas.edu.br>
2026-05-02 12:47:48 -0300

    Refactor verificar_titulo function for clarity
    Removi comentários 5 min

--- Códigos_fonte/validacoes/titulo.py
+++ Códigos_fonte/validacoes/titulo.py
@@ -1,25 +1,23 @@
 def verificar_titulo(titulo):
-    titulo = str(titulo).strip()  # Remove espaços em branco
-    titulo = ''.join(filter(str.isdigit, titulo))  # Remove caracteres não numéricos
+    titulo = str(titulo).strip()  
+    titulo = ''.join(filter(str.isdigit, titulo))  
     if len(titulo) != 12 or not titulo.isdigit():
             return False
-   #Separar as partes  
-    sequencial = titulo[:8]   # 8 primeiros dígitos
-    uf_digitos = titulo[8:10] # 9º e 10º dígitos (UF)
-    dv1 = int(titulo[10])     # 11º dígito (1º DV)
-    dv2 = int(titulo[11])     # 12º dígito (2º DV)
+   
+    sequencial = titulo[:8]   
+    uf_digitos = titulo[8:10] 
+    dv1 = int(titulo[10])     
+    dv2 = int(titulo[11])     
 
     uf = int(uf_digitos)
     soma1 = 0
 
-    # Pesos para o 1º DV: sequencial × [2,3,4,5,6,7,8,9]
     pesos1 = [2, 3, 4, 5, 6, 7, 8, 9]
     for i in range (8):
         soma1 += int(sequencial[i]) * pesos1[i]
 
     resto1 = soma1 % 11
 
-    # Regra especial SP (01) e MG (02): resto 0 → DV = 1
     if resto1 == 10:
         primeiro_dv = 0
 
@@ -36,7 +34,6 @@ def verificar_titulo(titulo):
         return False
 
 
-    # Pesos para o 2º DV: UF × [7,8] + 1º DV × 9
     soma2 = (int(uf_digitos[0]) * 7) + (int(uf_digitos[1]) * 8) + (primeiro_dv * 9)
     resto2 = soma2 % 11
 
