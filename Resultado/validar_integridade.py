da317c903f115998e3bd07b4cb9881a6289ef7d1
Seu Nome <luisa.mm1@puccampinas.edu.br>
2026-05-29 11:16:05 -0300

    criei docstring

--- Resultado/validar_integridade.py
+++ Resultado/validar_integridade.py
@@ -2,10 +2,29 @@ from conexao import conectar
 import os
 
 def limpar_tela():
+    """
+    Limpa o terminal de acordo com o sistema operacional em uso.
+
+    Args:
+        Nenhum.
+
+    Returns:
+        None.
+    """
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def relatorio_integridade():
-    
+    """
+    Verifica a integridade da eleição comparando o total de votos
+    registrados na tabela votos com o total de eleitores
+    que possuem status votou = 'S'.
+
+    Args:
+        Nenhum.
+
+    Returns:
+        None: Imprime o relatório de integridade no terminal.
+    """
     conexao = conectar()
     cursor = conexao.cursor(buffered=True)
 
