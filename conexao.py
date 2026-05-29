f385ceac1ce8fb44add2e479b84de496496f5f05
Seu Nome <luisa.mm1@puccampinas.edu.br>
2026-05-29 11:19:39 -0300

    criei docstring

--- conexao.py
+++ conexao.py
@@ -1,6 +1,16 @@
 import mysql.connector
 
 def conectar():
+    """
+    Cria e retorna uma conexão com o banco de dados MySQL do sistema.
+
+    Args:
+        Nenhum.
+
+    Returns:
+        mysql.connector.connection.MySQLConnection: Objeto de conexão
+        ativo com o banco de dados tabela_bd.
+    """
     return mysql.connector.connect(
         host='127.0.0.1',
         user='root',
